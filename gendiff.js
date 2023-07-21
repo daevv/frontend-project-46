545a88b7d81cb028d12d81405939cd85249a8ee5
daevv <dediaev.danil130702@gmail.com>
2023-07-21 11:12:21 +0300

    change gendiff.js

--- gendiff.js
+++ gendiff.js
@@ -5,7 +5,11 @@ import { program } from "commander";
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
-  .version('0.1.0');
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .version('0.1.0')
+  .option('-f, -format <type>', 'output format');
+  
 
 
 program.parse();
