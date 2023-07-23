616a4c72e15bb878ebe660dbcd2ed82e71bb5864
daevv <dediaev.danil130702@gmail.com>
2023-07-23 16:13:22 +0300

    add tests for yaml

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -2,19 +2,28 @@ import * as fs from 'node:fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'node:path';
+import yaml from 'js-yaml';
 
-import getComparison from '../src/json-comparator.js';
+import getComparison from '../src/comparator.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const expectedResult = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
 
-test('extractLinks', () => {
+test('json comparison', () => {
   const file1 = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
   const file2 = fs.readFileSync(getFixturePath('file2.json'), 'utf-8');
   const data1 = JSON.parse(file1);
   const data2 = JSON.parse(file2);
-  const expectedResult = fs.readFileSync(getFixturePath('expected.txt'), 'utf-8');
+  expect(getComparison(data1, data2)).toEqual(expectedResult);
+});
+
+test('yaml comparison', () => {
+  const file1 = fs.readFileSync(getFixturePath('file1.yml'), 'utf-8');
+  const file2 = fs.readFileSync(getFixturePath('file2.yml'), 'utf-8');
+  const data1 = yaml.load(file1);
+  const data2 = yaml.load(file2);
   expect(getComparison(data1, data2)).toEqual(expectedResult);
 });
