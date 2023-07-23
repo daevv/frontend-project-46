7f8412f062e5d47cbae5c2524e873735d01c713b
daevv <dediaev.danil130702@gmail.com>
2023-07-23 11:53:46 +0300

    fix bug with comparison

--- src/json-comparator.js
+++ src/json-comparator.js
@@ -5,18 +5,18 @@ const getComparison = (data1, data2) => {
   const allFields = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   for (let i = 0; i < allFields.length; i += 1) {
-    const currentFiled = allFields[i];
-    const data1Value = data1[currentFiled];
-    const data2Value = data2[currentFiled];
-    if (!data1Value) {
-      res.push(`  + ${currentFiled}: ${data2Value}`);
+    const currentField = allFields[i];
+    const data1Value = data1[currentField];
+    const data2Value = data2[currentField];
+    if (!_.has(data1, currentField)) {
+      res.push(`  + ${currentField}: ${data2Value}`);
+    } else if (!_.has(data2, currentField)) {
+      res.push(`  - ${currentField}: ${data1Value}`);
     } else if (data1Value !== data2Value) {
-      res.push(`  - ${currentFiled}: ${data1Value}`);
-      res.push(`  + ${currentFiled}: ${data2Value}`);
-    } else if (!data2Value) {
-      res.push(`  - ${currentFiled}: ${data1Value}`);
+      res.push(`  - ${currentField}: ${data1Value}`);
+      res.push(`  + ${currentField}: ${data2Value}`);
     } else {
-      res.push(`    ${currentFiled}: ${data1Value}`);
+      res.push(`    ${currentField}: ${data1Value}`);
     }
   }
   return `{\n${res.join('\n')}\n}`;
