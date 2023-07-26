9650006f2e2cc4aa62ffa8a488b26a169c651a44
daevv <dediaev.danil130702@gmail.com>
2023-07-26 16:20:31 +0300

    add new fixtures and test

--- __tests__/comparator.test.js
+++ __tests__/comparator.test.js
@@ -7,34 +7,33 @@ import yaml from 'js-yaml';
 import getComparison from '../src/comparator.js';
 import styler from '../src/formatters/stylish.js';
 import plain from '../src/formatters/plain.js';
+import json from '../src/formatters/json.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
 const expectedSylishResult = fs.readFileSync(getFixturePath('expected-stylish.txt'), 'utf-8');
 const expectedPlainResult = fs.readFileSync(getFixturePath('expected-plain.txt'), 'utf-8');
+const expectJsonResult = fs.readFileSync(getFixturePath('expected-json.json'), 'utf-8');
+const data1Json = JSON.parse(fs.readFileSync(getFixturePath('file1.json'), 'utf-8'));
+const data2Json = JSON.parse(fs.readFileSync(getFixturePath('file2.json'), 'utf-8'));
+const data1Yaml = yaml.load(fs.readFileSync(getFixturePath('file1.yml'), 'utf-8'));
+const data2Yaml = yaml.load(fs.readFileSync(getFixturePath('file2.yml'), 'utf-8'));
 
 test('json comparison', () => {
-  const file1 = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
-  const file2 = fs.readFileSync(getFixturePath('file2.json'), 'utf-8');
-  const data1 = JSON.parse(file1);
-  const data2 = JSON.parse(file2);
-  expect(getComparison(data1, data2, styler)).toEqual(expectedSylishResult);
+  expect(getComparison(data1Json, data2Json, styler)).toEqual(expectedSylishResult);
 });
 
 test('yaml comparison', () => {
-  const file1 = fs.readFileSync(getFixturePath('file1.yml'), 'utf-8');
-  const file2 = fs.readFileSync(getFixturePath('file2.yml'), 'utf-8');
-  const data1 = yaml.load(file1);
-  const data2 = yaml.load(file2);
-  expect(getComparison(data1, data2, styler)).toEqual(expectedSylishResult);
+  expect(getComparison(data1Yaml, data2Yaml, styler)).toEqual(expectedSylishResult);
 });
 
 test('plain format comparison', () => {
-  const file1 = fs.readFileSync(getFixturePath('file1.yml'), 'utf-8');
-  const file2 = fs.readFileSync(getFixturePath('file2.yml'), 'utf-8');
-  const data1 = yaml.load(file1);
-  const data2 = yaml.load(file2);
-  expect(getComparison(data1, data2, plain)).toEqual(expectedPlainResult);
+  expect(getComparison(data1Json, data2Yaml, plain)).toEqual(expectedPlainResult);
+});
+
+test('json format comparison', () => {
+  expect(getComparison(data1Json, data2Json, json)).toEqual(expectJsonResult);
 });
